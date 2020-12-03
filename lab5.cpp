a21276cf741a9abce643f00098ba30634022d858
Nataliapiesn <01123786@pw.edu.pl>
2020-12-03 19:12:02 +0000

    Vector of Humans. Task 4 completed.

--- lab5.cpp
+++ lab5.cpp
@@ -1,10 +1,19 @@
+#include "Human.hpp"
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int main()
 {
-    vector< int > vs{100, 42};
-
-    cout << vs.size() << " " << vs.capacity() << endl;
+    vector< Human > people;
+    for (int i = 0; i < 2; i++) {
+        std::string name;
+        cin >> name;
+        Human person(name, 0, true, true);
+        people.emplace_back(person);
+    }
+    cout << people.size() << " " << people.capacity() << endl;
+    for (int i = 0; i < 2; i++) {
+        cout << people[i].getName() << endl;
+    }
 }
