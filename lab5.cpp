5aaadc7e7b9dbf6d027ddb5eb0abc5cd02ffa058
Nataliapiesn <01123786@pw.edu.pl>
2020-12-03 18:49:06 +0000

    Task 2 completed.

--- lab5.cpp
+++ lab5.cpp
@@ -6,8 +6,9 @@ int main()
 {
     vector< int > vs;
     cout << vs.size() << " " << vs.capacity() << endl;
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 20; i++) {
         vs.push_back(i);
+        cout << &vs[0] << endl;
     }
     cout << vs.size() << " " << vs.capacity() << endl;
 }
