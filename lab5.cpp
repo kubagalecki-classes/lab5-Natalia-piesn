fa43545cc803ae1d7e54c47c10e53a813d488d86
Nataliapiesn <01123786@pw.edu.pl>
2020-12-03 20:14:04 +0000

    Task 13 completed.

--- lab5.cpp
+++ lab5.cpp
@@ -6,40 +6,40 @@ using namespace std;
 
 int main()
 {
+
+    list< int > numbers = {};
+
+    numbers.push_back(1);
+    numbers.push_back(2);
+
+    numbers.push_front(-1);
+    numbers.push_front(-3);
+    numbers.push_front(-2);
+    list< int >::iterator it = numbers.begin();
+    for (it = numbers.begin(); it != numbers.end(); it++) {
+        cout << *it << endl;
+    }
     /*
-      list< int > numbers = {};
-      cout << numbers.front() << " " << numbers.back() << endl;
-
-      numbers.push_back(1);
-      numbers.push_back(2);
-      cout << numbers.front() << " " << numbers.back() << endl;
-      cout << &numbers.front() << " " << &numbers.back() << endl;
-
-      numbers.push_front(-1);
-      numbers.push_front(-2);
-      numbers.push_front(-3);
-      cout << numbers.front() << " " << numbers.back() << endl;
-      cout << &numbers.front() << " " << &numbers.back() << endl;
-      */
-    vector< Human > people;
-    string          name;
-    int             a = 1;
-    while (a == 1) {
-        cin >> name;
-        if (name == "end") {
-            a = 0;
-        }
-        else if (name == "pop") {
-            people.pop_back();
-        }
-        else {
-            Human person(name, 0, true, true);
-            people.push_back(person);
-        }
-    }
-    cout << people.size() << " " << people.capacity() << endl;
-    vector< Human >::iterator it = people.begin();
-    for (it = people.begin(); it != people.end(); it++) {
-        cout << it->getName() << endl;
-    }
+  vector< Human > people;
+  string          name;
+  int             a = 1;
+  while (a == 1) {
+      cin >> name;
+      if (name == "end") {
+          a = 0;
+      }
+      else if (name == "pop") {
+          people.pop_back();
+      }
+      else {
+          Human person(name, 0, true, true);
+          people.push_back(person);
+      }
+  }
+  cout << people.size() << " " << people.capacity() << endl;
+  vector< Human >::iterator it = people.begin();
+  for (it = people.begin(); it != people.end(); it++) {
+      cout << it->getName() << endl;
+  }
+  */
 }
