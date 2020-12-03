71821fe97e6b5a300d4ae810be8b5fb700954aca
Nataliapiesn <01123786@pw.edu.pl>
2020-12-03 21:01:24 +0000

    Attempt 2

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -13,8 +13,13 @@ std::vector< char > foo(std::list< Human >& people)
   }
   vector<char> pet_likes;
   for (auto itr = people.crbegin(); itr != people.crend(); itr++) {
-    char ch = itr->isMonster();
-    pet_likes.push_back(ch);
+    if(itr->isMonster() == true){
+      pet_likes.push_back('n');
+    }
+    else
+    {
+      pet_likes.push_back('y');
+    }
   }
   return pet_likes;
 }
