953904418fb51502910092348e12790deea138cc
Nataliapiesn <01123786@pw.edu.pl>
2020-12-03 18:46:16 +0000

    Vector test. Task 1 completed.

--- lab5.cpp
+++ lab5.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
 int main()
 {
-    puts("Hello, World!");
+    vector< int > vs;
+    cout << vs.size() << " " << vs.capacity() << endl;
+    for (int i = 0; i < 100; i++) {
+        vs.push_back(i);
+    }
+    cout << vs.size() << " " << vs.capacity() << endl;
 }
